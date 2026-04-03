4359db487783672baa01903fd20e4af95f51e0d4
Moritz Johner <beller.moritz@googlemail.com>
2026-04-03 23:06:51 +0200

    chore: restore ebpf go:generate directive

--- internal/ebpf/gen.go
+++ internal/ebpf/gen.go
@@ -1,4 +1,3 @@
 package ebpf
 
-// Guarded until Task 8 introduces ../../bpf/egress.c.
-//go:generate sh -c 'test -f ../../bpf/egress.c && go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g" bpf ../../bpf/egress.c -- -I../../bpf/include || echo "skip bpf2go: ../../bpf/egress.c missing"'
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g" bpf ../../bpf/egress.c -- -I../../bpf/include
